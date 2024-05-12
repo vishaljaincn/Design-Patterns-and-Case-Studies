c3a16564bbced51b32bdcd09c6fdc32d181015c5
vishaljain <vishaljaincn@gmail.com>
2024-05-12 22:43:54 +0530

    Access Modifiers code pushed

---
+++ src/main/java/org/example/AccessModifiers2/Main.java
@@ -0,0 +1,14 @@
+package org.example.AccessModifiers2;
+
+
+public class Main {
+    public static void main(String[] args) {
+//        Student student = new Student();
+//        student.name = "Ajith";
+        // student.batchId = 10;
+        // student.age = 27;
+        // student.psp = 90.0;
+        System.out.println("Added something for git demo");
+    }
+
+}
---
+++ src/main/java/org/example/AccessModifiers2/StudentChild.java
@@ -0,0 +1,12 @@
+package org.example.AccessModifiers2;
+
+
+
+//public class StudentChild extends Student {
+////    StudentChild() {
+////        name = "Deepak";  //public
+////        //age = 27; //default
+////        // batchId = 10; //private
+////        psp = 90.0; // protected
+////    }
+//}
