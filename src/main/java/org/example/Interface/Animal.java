03633d2685553f03c720c6cd32d26f93f8d06b7b
vishaljain <vishaljaincn@gmail.com>
2024-05-14 16:42:25 +0530

    Interface code added

---
+++ src/main/java/org/example/Interface/Animal.java
@@ -0,0 +1,11 @@
+package org.example.Interface;
+
+public interface Animal {
+
+    //Methods are by default public in interface.
+    void eat(); // Abstract methods (Method without body)
+
+    void walk();
+
+    void run();
+}
---
+++ src/main/java/org/example/Interface/Client.java
@@ -0,0 +1,16 @@
+package org.example.Interface;
+
+
+public class Client {
+    public static void main(String[] args) {
+        // Cat cat = new Cat();
+        //Dog dog = new Dog();
+//        cat.walk();
+//        cat.run();
+//        cat.eat();
+
+        Animal animal = new Dog();
+        animal.eat();
+        animal.walk();
+    }
+}
