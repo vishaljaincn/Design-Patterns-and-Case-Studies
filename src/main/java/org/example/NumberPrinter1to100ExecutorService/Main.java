9b4a877779f8fcd5a9a7743c625efa7fa2b296c6
vishaljain <vishaljaincn@gmail.com>
2024-05-16 23:56:37 +0530

    Multithreading code

---
+++ src/main/java/org/example/NumberPrinter1to100ExecutorService/Main.java
@@ -0,0 +1,20 @@
+package org.example.NumberPrinter1to100ExecutorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        for (int i = 1; i <= 100; i++) {
+            if (i == 11 || i == 51 || i == 99) {
+                System.out.println("DEBUG");
+            }
+            NumberPrinter numberPrinter = new NumberPrinter(i);
+            executorService.submit(numberPrinter);
+        }
+
+        executorService.shutdown();
+    }
+}
---
+++ src/main/java/org/example/NumberPrinter1to100ExecutorService/NumberPrinter.java
@@ -0,0 +1,15 @@
+package org.example.NumberPrinter1to100ExecutorService;
+
+
+public class NumberPrinter implements Runnable {
+    private int numberToBePrinted;
+
+    NumberPrinter(int numberToBePrinted) {
+        this.numberToBePrinted = numberToBePrinted;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(numberToBePrinted + " Printed by thread: " + Thread.currentThread().getName());
+    }
+}
