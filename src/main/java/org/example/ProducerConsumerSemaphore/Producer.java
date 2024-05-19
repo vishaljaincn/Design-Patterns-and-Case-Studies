ec6093ab6b2723dedd9fc4267425c52966f4a957
vishaljain <vishaljaincn@gmail.com>
2024-05-20 02:22:22 +0530

    Semaphore code added

---
+++ src/main/java/org/example/ProducerConsumerSemaphore/Producer.java
@@ -0,0 +1,37 @@
+package org.example.ProducerConsumerSemaphore;
+
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+
+public class Producer implements Runnable {
+    private Queue<Object> queue;
+    private String name;
+    private int maxSize;
+    private Semaphore producerSemaphore;
+    private Semaphore consumerSemaphore;
+
+    Producer(Queue<Object> queue, int maxSize, String name,
+             Semaphore producerSemaphore, Semaphore consumerSemaphore) {
+        this.queue = queue;
+        this.name = name;
+        this.maxSize = maxSize;
+        this.producerSemaphore = producerSemaphore;
+        this.consumerSemaphore = consumerSemaphore;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+//            if (queue.size() < maxSize) {
+            try {
+                producerSemaphore.acquire(); // ps--
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(name + "is producing an element & size is " + queue.size());
+            queue.add(new Object());
+            consumerSemaphore.release(); // cs++
+//            }
+        }
+    }
+}
