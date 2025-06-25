51184ba29f8433a5e0b4832169efc557416aba56
vishaljain <vishaljaincn@gmail.com>
2025-06-25 20:17:13 +0530

    Refactor RemoveDuplicates class; remove unused imports and simplify duplicate removal logic

--- src/main/java/org/example/java8practicequestions/RemoveDuplicates.java
+++ src/main/java/org/example/java8practicequestions/RemoveDuplicates.java
@@ -2,8 +2,6 @@ package org.example.java8practicequestions;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
@@ -13,12 +11,9 @@ public class RemoveDuplicates {
         listWithDuplicates.add("apple");
         listWithDuplicates.add("orange");
         listWithDuplicates.add("banana");
-        Map<String, Long> map = listWithDuplicates.stream().collect(Collectors.groupingBy((i -> i), Collectors.counting()));
-
         List<String> listWithoutDuplicates = listWithDuplicates.stream()
                 .distinct()
                 .toList();
-
         System.out.println("List with duplicates: " + listWithDuplicates);
         System.out.println("List without duplicates: " + listWithoutDuplicates);
     }
