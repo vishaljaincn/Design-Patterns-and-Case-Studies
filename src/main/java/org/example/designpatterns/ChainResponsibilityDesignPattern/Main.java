2e0bdf461490a85654cf985b870e3b132f9ac666
vishaljain <vishaljaincn@gmail.com>
2025-07-03 22:00:19 +0530

    Uncomment log statements in Main class to enable error and debug logging

--- src/main/java/org/example/designpatterns/ChainResponsibilityDesignPattern/Main.java
+++ src/main/java/org/example/designpatterns/ChainResponsibilityDesignPattern/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
         LogProcessor logObject = new InfoLogProcessor(new DebugLogProcessor(new ErrorLogProcessor(null)));
 
-//        logObject.log(LogProcessor.ERROR, "exception happens");
-//        logObject.log(LogProcessor.DEBUG, "need to debug this ");
+        logObject.log(LogProcessor.ERROR, "exception happens");
+        logObject.log(LogProcessor.DEBUG, "need to debug this ");
         logObject.log(LogProcessor.INFO, "just for info ");
     }
 }
