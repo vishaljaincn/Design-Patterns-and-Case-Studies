ec35d1e18545c243088126c415a5d8a3159030a8
vishaljain <vishaljaincn@gmail.com>
2025-07-12 01:35:36 +0530

    Rename Deloitte class to BinarySearchDeloitte for clarity and specificity in functionality

--- src/main/java/org/example/java8practicequestions/BinarySearchDeloitte.java
+++ src/main/java/org/example/java8practicequestions/BinarySearchDeloitte.java
@@ -1,6 +1,6 @@
 package org.example.java8practicequestions;
 
-public class Deloitte {
+public class BinarySearchDeloitte {
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 4, 5};
         System.out.println(binarySearch(a, 9000));
